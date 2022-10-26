2e6587e8bde2fe06afd5603c340c87aef691c50b
Ayu Istikomah <ayuisti29@gmail.com>
2022-10-26 09:23:45 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama ="Ayu Istikomah";
+$umur =20;
+
+echo "Nama Saya <strong>$nama, saya berusia $umur tahun.</br>";
+
+$namaAdik ="Setya";
+$umurAdik ="2";
+
+echo "Nama  adik saya <strong>$namaAdik</strong>, adik saya berusia
+$umurAdik tahun.<br>";
+
+$selisihumur = $umur - $umurAdik;
+echo "Selisih usia saya dan adik saya adalah $selisihumur tahun.";
+
+?>
